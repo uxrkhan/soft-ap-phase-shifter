fc56b9341ceaa1b2bc0c36833d8ff2ec55424ee3
uxrkhan <uzairk365@gmail.com>
2021-03-29 12:29:00 +0530

    Update test cases

--- test/dac_test/all_dac_test/all_dac_test.ino
+++ test/dac_test/all_dac_test/all_dac_test.ino
@@ -1,17 +1,20 @@
 #include <Wire.h>
 
-#define dac1 0x62
-#define dac2 0x63  
+#define dac1A 0x60  // different manufacturer
+#define dac1B 0x63  // soldered
+#define dac2A 0x62  // default
+#define dac2B 0x63  // soldered
 
-float voltages[] = {1, 2, 2.5, 3.3};
+float voltages[] = {1, 2, 2.5, 3};
 int vals[4];
 
 void setup() {
   Wire.begin();
-  Wire1.begin(18,19,100000);
+  Wire1.begin(14,13);
 }
 
 void send1(byte addr, int val) {
+  Wire.begin();
   Wire.beginTransmission(addr);
   Wire.write(64);
   Wire.write(val >> 4);
@@ -20,6 +23,7 @@ void send1(byte addr, int val) {
 }
 
 void send2(byte addr, int val) {
+  Wire1.begin(14,13);
   Wire1.beginTransmission(addr);
   Wire1.write(64);
   Wire1.write(val >> 4);
@@ -32,8 +36,8 @@ void loop()
   for (int i = 0; i < 4; i++) {
     vals[i] = ceil(voltages[i] * (4095/3.3));
   }
-  send1(dac1, vals[0]);
-  send1(dac2, vals[1]);
-  send2(dac1, vals[2]);
-  send2(dac2, vals[3]); 
+  send1(dac1A, vals[0]);
+  send1(dac1B, vals[1]);
+  send2(dac2A, vals[2]);
+  send2(dac2B, vals[3]); 
 }
--- test/opamp_test/opamp_test.ino
+++ test/opamp_test/opamp_test.ino
@@ -1,16 +1,15 @@
-// This is a program to test a single MCP4725 DAC 
-// Enter the input voltage in the terminal and the program will display the corresponding voltages. 
-
 #include <Wire.h>
 
-#define ADDR 0x62   // dac address (0x63 -> dac with short circuit)
-#define VCC 3.26    // measure voltage across dac VCC and enter here.
-#define GAIN 3.7    // op-amp gain
+#define ADDR 0x62
+#define VCC 3.27
+#define GAIN 3.2
 
 void setup() {
-  Wire.begin();
-  Wire1.begin(14,13); // sda, scl 
+  pinMode(2,OUTPUT);
   Serial.begin(115200);
+  Wire.begin();
+  Wire1.begin(14,13);
+  delay(1000);
 }
 
 void send(byte addr, int val) {
@@ -30,19 +29,16 @@ void send2(byte addr, int val) {
 }
 
 void loop() {
-  Serial.print("Enter voltage. ");
-  while(Serial.available()==0) { 
+  Serial.print("Enter voltage: ");
+  while (Serial.available() == 0) {
+    
   }
   String voltage_str = Serial.readString();
   Serial.println(voltage_str);
-
-  Serial.println("--------------");
-  Serial.print("VOLTAGE ENTERED: ");
-  Serial.print(voltage_str);
-
-  float voltage = voltage_str.toFloat();
+  Serial.println("Voltage entered: " + voltage_str + " V");
   
-  Serial.print("FLOAT VOLTAGE:   ");
+  float voltage = voltage_str.toFloat();
+  Serial.print("Float voltage: ");
   Serial.println(voltage);
 
   int val = int(voltage * 4096 / 3.3);
@@ -50,9 +46,9 @@ void loop() {
     val = 4095;
   if (val < 0)
     val = 0;
+
   float dac_op = val * VCC / 4096;
   float output = dac_op * GAIN;
-
   Serial.print("INDEX VALUE:     ");
   Serial.println(val);
   Serial.print("DAC OUTPUT:      ");
@@ -62,6 +58,5 @@ void loop() {
   Serial.println("--------------");
 
   send(ADDR, val);
-  send2(ADDR, val);
-  Serial.println("Voltage successfully transferred.\n");
+  Serial.println("Voltage successfully transferrred.");
 }
