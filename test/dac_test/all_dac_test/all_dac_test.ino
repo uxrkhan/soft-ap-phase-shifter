dc23b55a2a052cd1fff47e6ad18f707b29d6536f
uxrkhan <uzairk365@gmail.com>
2021-04-02 13:26:11 +0530

    Some testing changes

--- test/dac_test/all_dac_test/all_dac_test.ino
+++ test/dac_test/all_dac_test/all_dac_test.ino
@@ -9,12 +9,22 @@ float voltages[] = {1, 2, 2.5, 3};
 int vals[4];
 
 void setup() {
+  Serial.begin(115200);
   Wire.begin();
   Wire1.begin(14,13);
+
+  for (int i = 0; i < 4; i++) {
+    vals[i] = floor(voltages[i] * (4095/11.65));
+    float Vdac = voltages[i] / 11.65 * 3.3;
+    Serial.println(String(vals[i]) + " " + String(Vdac));
+  }
+  send1(dac1A, vals[0]);
+  send1(dac1B, vals[1]);
+  send2(dac2A, vals[2]);
+  send2(dac2B, vals[3]);
 }
 
 void send1(byte addr, int val) {
-  Wire.begin();
   Wire.beginTransmission(addr);
   Wire.write(64);
   Wire.write(val >> 4);
@@ -23,7 +33,6 @@ void send1(byte addr, int val) {
 }
 
 void send2(byte addr, int val) {
-  Wire1.begin(14,13);
   Wire1.beginTransmission(addr);
   Wire1.write(64);
   Wire1.write(val >> 4);
@@ -33,11 +42,5 @@ void send2(byte addr, int val) {
 
 void loop()
 {  
-  for (int i = 0; i < 4; i++) {
-    vals[i] = ceil(voltages[i] * (4095/3.3));
-  }
-  send1(dac1A, vals[0]);
-  send1(dac1B, vals[1]);
-  send2(dac2A, vals[2]);
-  send2(dac2B, vals[3]); 
+   
 }
